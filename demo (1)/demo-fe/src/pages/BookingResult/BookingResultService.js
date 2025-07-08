ef0600c3605d8ce23f9f3c0324ecc45ac05e6ce2
NgoDuyHung2305 <22520511@gm.uit.edu.vn>
2025-07-08 13:14:49 +0700

    refactor

--- demo (1)/demo-fe/src/pages/BookingResult/BookingResultService.js
+++
@@ -1,4 +0,0 @@
-import apiClient from "../../APIService";
-export const getBookingByCode = (bookingCode) => { 
-    return apiClient.get(`/guest-bookings/code/${bookingCode}`);
-};
--- demo (1)/demo-fe/src/pages/Home/HomeService.js
+++
@@ -1,9 +0,0 @@
-import apiClient from "../../APIService";
-
-export const getNowShowingMovies = () => {
-  return apiClient.get('/movies/now-showing');
-};
-
-export const getComingSoonMovies = () => {
-  return apiClient.get('/movies/coming-soon');
-};
